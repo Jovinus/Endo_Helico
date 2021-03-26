33fcfecfccb6a82598c4b4a715ac78d97cd92c1e
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-03-26 11:36:13 +0900

    add more code

--- code/basic_stats.py
+++ code/basic_stats.py
@@ -9,8 +9,14 @@ pd.set_option("display.max_columns", None)
 df_init = datatable.fread('../prepro_data/h_pylori_tmp.csv', na_strings=['NA', ''], encoding='utf-8-sig').to_pandas()
 df_init['SM_DATE'] = df_init['SM_DATE'].astype('datetime64')
 df_init['year'] = df_init['SM_DATE'].dt.year
+df_init['num_case_year'] = df_init.groupby(['year'])['year'].transform(lambda x: len(x))
 df_init.head()
-# %%
-display(df_init.groupby(['year'])['h_pyl_positive'].value_counts(dropna=False))
-display(df_init.groupby(['year'])['h_pyl_positive'].value_counts(dropna=False, normalize=True))
+# %% Yearly pylori occurence
+stat_1 = df_init.groupby(['year', 'num_case_year'])['h_pyl_positive'].value_counts(dropna=False)
+stat_2 = df_init.groupby(['year', 'num_case_year'])['h_pyl_positive'].value_counts(dropna=False, normalize=True)
+
+display(stat_1, stat_2)
+# %% Save stats
+stat_1.reset_index(name='count').to_csv('../prepro_data/stat_1.csv', encoding='utf-8-sig', index=False)
+stat_2.reset_index(name='percent').to_csv('../prepro_data/stat_2.csv', encoding='utf-8-sig', index=False)
 # %%
--- code/data_preprocessing.py
+++ code/data_preprocessing.py
@@ -6,19 +6,27 @@ from IPython.display import display
 pd.set_option("display.max_columns", None)
 # %%
 WORK_PATH = "/Users/lkh256/Studio/Endo_Helico"
+
+#### Read script data that we want to classify 
 df_script = datatable.fread(os.path.join(WORK_PATH, "data/endo_biopsy_2008to2020.csv"), 
                             na_strings=['', 'NA'], encoding='utf-8-sig').to_pandas()
-df_script['SM_DATE'] = df_script['처방일자#5'].astype('datetime64')
-df_script['EXEC_TIME'] = df_script['시행일시#6'].astype('datetime64')
+
+#### Change data type to datetime64 from object
+column_mask = ['처방일자#5', '시행일자#6']
+df_script[['SM_DATE', 'EXEC_TIME']] = df_script[column_mask].astype('datetime64')
 print("Number of case = {}".format(len(df_script)))
 display(df_script.head())
 
+
+#### Read label data
 df_results = datatable.fread(os.path.join(WORK_PATH, "data/heli_results_2008to2020.csv"),
                              na_strings=['', 'NA'], encoding='utf-8-sig').to_pandas()
+#### Change data type to datetime64 from object
 df_results['SM_DATE'] = df_results['처방일자#2'].astype('datetime64')
 print("Number of case = {}".format(len(df_results)))
 display(df_results.head())
 # %% Data Sample 
+#### Plot sample data
 print(df_script['검사결과내용#9'].head(1).values)
 # %% Check result dataset
 df_results['건강검진결과명#6'].value_counts()
@@ -26,16 +34,20 @@ df_results['건강검진결과명#6'].value_counts()
 df_all = pd.merge(df_script, df_results, how='left', left_on=['환자번호#1', 'SM_DATE'],
                   right_on=['환자번호#1', 'SM_DATE'])
 
+
 #### Assign 1 if h.pylori is present from biopsy, 0 for less
 df_all['h_pyl_positive'] = df_all['건강검진결과명#6'].notnull().astype(int)
 display(df_all.head())
+
 # %% Drop the unnesseary columns
 col_to_drop = ['처방일자#2', '검사코드#3', '검사코드명#4', '건강검진결과코드#5', '건강검진결과명#6', '결론진단내용#10', '처방일자#5']
 df_all.drop(columns=col_to_drop, inplace=True)
 display(df_all.head())
+
 # %% Change column order and name
 col_nm_to_ch = {'환자번호#1':'ID', '검사결과내용#9':'result_text', '처방코드#3':'처방코드'}
 df_all.rename(columns=col_nm_to_ch, inplace=True)
+
 # %%
 col_to_select = ['ID', 'SM_DATE', 'EXEC_TIME','처방코드', 'result_text', 'h_pyl_positive']
 df_select = df_all[col_to_select].copy()
@@ -45,6 +57,7 @@ df_select.head()
 
 ### temporary data saving for stats
 df_select.to_csv(os.path.join(WORK_PATH, 'prepro_data/h_pylori_tmp.csv'), index=False, encoding='utf-8-sig')
+
 # %% Strip the unnecessary patterns
 
 #### Add pattern to strip. Below codes are just brief ones.
